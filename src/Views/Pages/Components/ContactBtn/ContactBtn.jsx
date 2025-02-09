34236f5b82c5964f586f28a4a73dd997cf0a0c77
Dhaval Patel <rnwdhaval1@gmail.com>
2025-02-09 12:54:08 +0530

    button added in contact

---
+++ src/Views/Pages/Components/ContactBtn/ContactBtn.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./ContactBtn.css"
+
+const ContactBtn = () => {
+    return (
+        <>
+            <button className='contact-btn mt-3 text-uppercase' data-aos="fade-up" data-aos-delay="500">
+                <span>Contact Now</span>
+            </button>
+
+        </>
+    )
+}
+
+export default ContactBtn
--- src/Views/Pages/Home/Section5.jsx
+++ src/Views/Pages/Home/Section5.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ContactBtn from '../Components/ContactBtn/ContactBtn'
 
 const Section5 = () => {
     return (
@@ -6,10 +7,11 @@ const Section5 = () => {
             <div className="container">
                 <h2 className='fw-bold display-4 mb-5' data-aos="fade-up" data-aos-delay="100">Want a unique solution to meet your needs?</h2>
                 <div className="row py-5 border rounded bg-img2">
-                    <div className="col-md-12 py-5   ">
-                        <div className="container  ">
+                    <div className="col-md-12 py-5">
+                        <div className="container" data-aos="fade-up" data-aos-delay="100">
                             {/* <span className='display-5'>Transforming Talent into Success</span><br /> */}
-                            <span className='text-shadow display-5'> Your Trusted Executive Search & <br /> <span className='text-warning'>Recruitment Partner.</span></span><br />
+                            <span className='text-shadow display-5 capitalize'> Your Trusted Executive Search & <br /> <span className='text-warning'>Recruitment Partner.</span></span><br />
+                            <ContactBtn />
                         </div>
                     </div>
                 </div>
